979eca96277429b68b9a6f39cff7c0a4dd58e90d
k-fujita <k-fujita@ist.osaka-u.ac.jp>
2014-01-26 23:46:04 +0900

    first commit

---
+++ .emacs.d/init.el
@@ -0,0 +1,237 @@
+;;; -*- Mode: Emacs-Lisp ; coding: iso-2022-jp-unix -*-;;;
+
+;; ----------------------------------------------------
+;; 基本設定
+;; ----------------------------------------------------
+
+; first directory
+(cd "~/")
+
+; load-path
+(add-to-list 'load-path "~/.emacs.d/site-lisp")
+
+; 言語
+(set-language-environment "Japanese")
+
+; 文字コード
+(require 'ucs-normalize)
+(prefer-coding-system 'utf-8)
+(setq file-name-coding-system 'utf-8-hfs)
+(setq local-coding-system 'utf-8-hfs)
+
+; 起動時の画面はいらない
+(setq inhibit-startup-message t)
+
+; スクロールで改行しない
+(setq next-line-add-newlines nil)
+
+; ツールバー、メニューバー非表示
+(if window-system
+    (tool-bar-mode 0))
+
+; タイトルバーにファイルのフルパス表示
+(setq frame-title-format
+      (format "%%f - Emacs@%s" (system-name)))
+
+; 画面スクロール時の重複行数
+(setq next-screen-context-lines 1)
+
+
+;; ----------------------------------------------------
+;; 表示関連
+;; ----------------------------------------------------
+
+; 行番号を左に表示f
+(require 'linum)
+(global-linum-mode t)
+(set-face-attribute 'linum nil :foreground "IndianRed" :height 0.8)
+(setq linum-format "%4d ")
+
+; 時間を表示
+(display-time-mode 1)
+
+; 行番号をバーに表示
+(column-number-mode 1)
+
+; ビープ音を鳴らさない
+(setq visible-bell t)
+
+; 対応する括弧を光らせる。
+(show-paren-mode t)
+(setq show-paren-delay 0)
+(setq show-paren-style 'expression)
+
+; C-x C-f での意味の無いパス表示をグレーアウトする
+(file-name-shadow-mode t)
+
+; scratchバッファの初期メッセージを消す
+(setq initial-scratch-message "")
+
+; yes/no を y/n へ簡略化
+(fset 'yes-or-no-p 'y-or-n-p)
+
+; リージョンをハイライト
+(setq-default transient-mark-mode t)
+
+; 行間を指定
+(setq-default line-spacing 0.2)
+
+; バッファ画面外文字の切り詰め表示
+(setq truncate-lines nil)
+
+; 行末の空白を強調表示
+(setq-default show-trailing-whitespace t)
+(set-face-background 'trailing-whitespace "#b14770")
+
+; ウィンドウ縦分割時のバッファ画面外文字の切り詰め表示
+(setq truncate-partial-width-windows t)
+
+;; ----------------------------------------------------
+;; Look and Feel
+;; ----------------------------------------------------
+
+; theme
+(if window-system (load-theme 'deeper-blue t))
+; solalized
+;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
+;; (if window-system (load-theme 'solarized-dark t))
+
+; 透過
+(if window-system (set-frame-parameter nil 'alpha 95))
+
+; mode line 平面化
+(set-face-attribute 'mode-line          nil :box nil)
+(set-face-attribute 'mode-line-inactive nil :box nil)
+
+;; ----------------------------------------------------
+;; フォント関連
+;; ----------------------------------------------------
+
+; 出典：http://sakito.jp/emacs/emacs23.html
+(if (eq window-system 'ns)
+    (progn
+      (create-fontset-from-ascii-font "Monaco-12:weight=normal:slant=normal" nil "menlokakugo")
+      (set-fontset-font "fontset-menlokakugo"
+                        'unicode
+                        (font-spec :family "Hiragino Kaku Gothic ProN" :size 14)
+                        nil
+                        'append)
+      (add-to-list 'default-frame-alist '(font . "fontset-menlokakugo"))
+
+      ))
+
+;; ----------------------------------------------------
+;; 文字関連
+;; ----------------------------------------------------
+
+; Tab幅4
+(setq-default tab-width 4)
+
+; インデントはTabではなくSpace
+(setq-default indent-tabs-mode nil)
+
+; インデントは4スペース
+(setq-default c-basic-offset 4)
+
+; 補完時に大文字小文字を区別しない
+(setq completion-ignore-case t)
+
+; リージョンがactiveな時の挙動をWindowsと同じにする
+(delete-selection-mode 1)
+
+; 行の先頭でC-kを一回押すだけで行全体を消去する
+(setq kill-whole-line t)
+
+; 最終行に必ず一行挿入する
+(setq require-final-newline t)
+
+; 複数の whitespace を削除
+(setq backward-delete-char-untabify-method 'hungry)
+
+; バッファ切り替え時にリージョンを保持
+(setq highlight-nonselected-windows t)
+
+;; ----------------------------------------------------
+;; キーバインド
+;; ----------------------------------------------------
+
+; CUA mode
+;(cua-mode t)
+;(setq cua-enable-cua-keys nil)
+
+; 補完
+(define-key global-map (kbd "C-c C-i") 'dabbrev-expand)
+
+; undo
+(define-key global-map (kbd "C-z") 'undo)
+
+; goto-line
+(define-key global-map (kbd "M-g") 'goto-line)
+
+; ある程度 Mac 標準に準拠させる
+(setq mac-command-key-is-meta nil)
+(setq mac-option-modifier 'meta)
+(setq mac-command-modifier 'super)
+(global-set-key [?\s-c] 'kill-ring-save)
+(global-set-key [?\s-v] 'yank)
+(global-set-key [?\s-x] 'kill-region)
+(global-set-key [?\s-z] 'undo)
+(global-set-key [?\s-s] 'save-buffer)
+(global-set-key [?\s-q] 'save-buffers-kill-terminal)
+(global-set-key [?\s-f] 'isearch-forward)
+(global-set-key [?\s-g] 'isearch-repeat-forward)
+(global-set-key "\C-h" 'delete-backward-char)
+(global-set-key "\M-h" 'help)
+
+; コピー アンド ペースト
+;; system-type predicates
+;; from http://d.hatena.ne.jp/tomoya/20090807/1249601308
+(setq darwin-p   (eq system-type 'darwin)
+      linux-p    (eq system-type 'gnu/linux)
+      carbon-p   (eq system-type 'mac)
+      meadow-p   (featurep 'meadow))
+
+; Emacs と Mac のクリップボード共有
+; from http://hakurei-shain.blogspot.com/2010/05/mac.html
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(if (or darwin-p carbon-p)
+  (setq interprogram-cut-function 'paste-to-osx)
+  (setq interprogram-paste-function 'copy-from-osx))
+
+;; ----------------------------------------------------
+;; バックアップ関連
+;; ----------------------------------------------------
+
+; バックアップファイルをまとめる
+(setq make-backup-files t)
+(setq backup-directory-alist
+      (cons (cons "\\.*$" (expand-file-name "~/.emacs.d/backup"))
+            backup-directory-alist))
+
+; 複数のバックアップ世代を管理
+(setq version-control t)
+
+; 新しいものをいくつ残すか
+(setq kept-new-versions 5)
+
+; 古いものをいくつ残すか
+(setq kept-old-versions 5)
+
+; 確認せずに古いものを消す
+(setq delete-old-versions t)
+
+; バージョン管理下のファイルもバックアップを作る
+(setq vc-make-backup-files t)
+
+; Dropbox
+(setq auto-save-file-name-transforms
+      `((".*/Dropbox/.*" ,temporary-file-directory t)))
+
---
+++ .zshrc
@@ -0,0 +1,184 @@
+# # ----------------------------
+# # aliases #
+# # ----------------------------
+
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+
+# LS の色
+eval $(dircolors .dircolors-solarized/dircolors.256dark)
+
+alias ls='ls --color=auto'
+alias la='ls -Alh'
+alias ll='ls -lh'
+alias l=ls
+alias p=pushd pp=popd
+alias df='df -h'
+alias du='du -h'
+
+export JAVA_HOME="/Library/Internet Plug-ins/JavaAppletPlugin.plugin/Contents/Home"
+export JAVA=$JAVA_HOME/bin
+
+# # ------------------------------
+# # General Settings
+# # ------------------------------
+
+export EDITOR=emacs      # エディタをemacsに設定
+export LANG=ja_JP.UTF-8  # 文字コードをUTF-8に設定
+export KCODE=u           # KCODEにUTF-8を設定
+export AUTOFEATURE=true  # autotestでfeatureを動かす
+
+bindkey -e               # キーバインドをemacsモードに設定
+
+setopt no_beep           # ビープ音を鳴らさないようにする
+setopt auto_cd           # ディレクトリ名の入力のみで移動する 
+setopt auto_pushd        # cd時にディレクトリスタックにpushdする
+setopt correct           # コマンドのスペルを訂正する
+setopt magic_equal_subst # =以降も補完する(--prefix=/usrなど)
+setopt prompt_subst      # プロンプト定義内で変数置換やコマンド置換を扱う
+setopt notify            # バックグラウンドジョブの状態変化を即時報告する
+setopt equals            # =commandを`which command`と同じ処理にする
+
+# Complement #
+autoload -Uz compinit    # 補完機能を有効にする
+compinit
+setopt auto_list         # 補完候補を一覧で表示する(d)
+setopt auto_menu         # 補完キー連打で補完候補を順に表示する(d)
+setopt list_packed       # 補完候補をできるだけ詰めて表示する
+setopt list_types        # 補完候補にファイルの種類も表示する
+bindkey "^[[Z" reverse-menu-complete                # Shift-Tabで補完候補を逆順する("\e[Z"でも動作する)
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}' # 補完時に大文字小文字を区別しない
+
+# Glob #
+setopt extended_glob # グロブ機能を拡張する
+unsetopt caseglob    # ファイルグロブで大文字小文字を区別しない
+
+# History #
+HISTFILE=~/.zsh_history   # ヒストリを保存するファイル
+HISTSIZE=10000            # メモリに保存されるヒストリの件数
+SAVEHIST=10000            # 保存されるヒストリの件数
+setopt bang_hist          # !を使ったヒストリ展開を行う(d)
+setopt extended_history   # ヒストリに実行時間も保存する
+setopt hist_ignore_dups   # 直前と同じコマンドはヒストリに追加しない
+setopt share_history      # 他のシェルのヒストリをリアルタイムで共有する
+setopt hist_reduce_blanks # 余分なスペースを削除してヒストリに保存する
+
+# マッチしたコマンドのヒストリを表示できるようにする #
+autoload history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+bindkey "^P" history-beginning-search-backward-end
+bindkey "^N" history-beginning-search-forward-end
+
+# すべてのヒストリを表示する #
+function history-all { history -E 1 }
+
+# PROMPT
+#################################################
+# 色一覧
+#################################################
+# 00: なにもしない
+# 01: 太字化
+# 04: 下線
+# 05: 点滅
+# 07: 前背色反転
+# 08: 表示しない
+# 22: ノーマル化
+# 24: 下線なし
+# 25: 点滅なし
+# 27: 前背色反転なし
+# 30: 黒(前景色)
+# 31: 赤(前景色)
+# 32: 緑(前景色)
+# 33: 茶(前景色)
+# 34: 青(前景色)
+# 35: マゼンタ(前景色)
+# 36: シアン(前景色)
+# 37: 白(前景色)
+# 39: デフォルト(前景色)
+# 40: 黒(背景色)
+# 41: 赤(背景色)
+# 42: 緑(背景色)
+# 43: 茶(背景色)
+# 44: 青(背景色)
+# 45: マゼンタ(背景色)
+# 46: シアン(背景色)
+# 47: 白(背景色)
+# 49: デフォルト(背景色)
+
+#################################################
+# プロンプト表示フォーマット
+# http://zsh.sourceforge.net/Doc/Release/zsh_12.html#SEC40
+#################################################
+# %% %を表示
+# %) )を表示
+# %l 端末名省略形
+# %M ホスト名(FQDN)
+# %m ホスト名(サブドメイン)
+# %n ユーザー名
+# %y 端末名
+# %# rootなら#、他は%を表示
+# %? 直前に実行したコマンドの結果コード
+# %d ワーキングディレクトリ %/ でも可
+# %~ ホームディレクトリからのパス
+# %h ヒストリ番号 %! でも可
+# %a The observed action, i.e. "logged on" or "logged off".
+# %S (%s) 反転モードの開始/終了 %S abc %s とするとabcが反転
+# %U (%u) 下線モードの開始/終了 %U abc %u とするとabcに下線
+# %B (%b) 強調モードの開始/終了 %B abc %b とするとabcを強調
+# %t 時刻表示(12時間単位、午前/午後つき) %@ でも可
+# %T 時刻表示(24時間表示)
+# %* 時刻表示(24時間表示秒付き)
+# %w 日表示(dd) 日本語だと 曜日 日
+# %W 年月日表示(mm/dd/yy)
+# %D 年月日表示(yy-mm-dd)
+
+# プロンプトに色を付ける #
+autoload -U colors; colors
+
+# 一般ユーザ時 #
+tmp_prompt="%{${fg[magenta]}%}%n@%m${WINDOW:+"[$WINDOW]"} %B%(?,%{${fg[green]}%},%{${fg[red]}%})%(!,#,>)%b %{${reset_color}%}"
+tmp_prompt2="%{${fg[magenta]}%}%_> %{${reset_color}%}"
+tmp_rprompt="%{${fg[green]}%}[%~] %{${fg[yellow]}%}[%W %T]%{${reset_color}%}"
+tmp_sprompt="%{${fg[yellow]}%}%r is correct? [Yes, No, Abort, Edit]:%{${reset_color}%}"
+
+# rootユーザ時(太字にし、アンダーバーをつける) #
+if [ ${UID} -eq 0 ]; then
+  tmp_prompt="%B%U${tmp_prompt}%u%b"
+  tmp_prompt2="%B%U${tmp_prompt2}%u%b"
+  tmp_rprompt="%B%U${tmp_rprompt}%u%b"
+  tmp_sprompt="%B%U${tmp_sprompt}%u%b"
+fi
+
+PROMPT=$tmp_prompt    # 通常のプロンプト
+PROMPT2=$tmp_prompt2  # セカンダリのプロンプト(コマンドが2行以上の時に表示される)
+RPROMPT=$tmp_rprompt  # 右側のプロンプト
+SPROMPT=$tmp_sprompt  # スペル訂正用プロンプト
+
+# SSHログイン時のプロンプト #
+[ -n "${REMOTEHOST}${SSH_CONNECTION}" ] &&
+  PROMPT="%{${fg[red]}%}%n@%m${WINDOW:+"[$WINDOW]"} %B%(?,%{${fg[green]}%},%{${fg[red]}%})%(!,#,>)%b %{${reset_color}%}"
+;
+
+# Title (user@hostname) #
+case "${TERM}" in
+kterm*|xterm*|)
+  precmd() {
+    echo -ne "\033]0;${USER}@${HOST%%.*}\007"
+  }
+  ;;
+esac
+
+
+# # ------------------------------
+# # Other Settings
+# # ------------------------------
+
+function chpwd() { ls }
+
+function print_known_hosts (){
+  if [ -f $HOME/.ssh/known_hosts ]; then
+    cat $HOME/.ssh/known_hosts | tr ',' ' ' | cut -d' ' -f1
+  fi  
+}
+_cache_hosts=($( print_known_hosts ))
+
