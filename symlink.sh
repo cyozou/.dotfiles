0f3132e132818ca303325b056164d2b282edb46f
dec12 <dec12.kaz@gmail.com>
2014-02-06 18:06:00 +0900

    make symlink.sh

---
+++ symlink.sh
@@ -0,0 +1,6 @@
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.screenrc ~/.screenrc
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.emacs.d/init.el ~/.emacs.d/init.el
+ln -s ~/.dotfiles/.emacs.d/themes ~/.emacs.d/themes
+ln -s ~/.dotfiles/.emacs.d/site-lisp ~/.emacs.d/site-lisp
